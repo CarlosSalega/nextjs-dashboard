7fe0d3442a83beab8086668e9741818193d4bcb6
Carlos Salega <carlosalfredosalega@gmail.com>
2025-01-12 06:45:26 -0300

    fix(config): fix font config ✏️

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,14 +3,17 @@ import { Poppins, Roboto, Lusitana } from "next/font/google";
 export const poppins = Poppins({
 	weight: ["400", "500", "600", "800"],
 	style: "normal",
+	subsets: ["latin-ext"],
 });
 
 export const roboto = Roboto({
 	weight: ["400", "500", "700"],
 	style: "normal",
+	subsets: ["latin-ext"],
 });
 
 export const lusitana = Lusitana({
 	weight: ["400", "700"],
 	style: "normal",
+	subsets: ["latin"],
 });
