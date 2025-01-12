82137b70aa0f7c95dab52255dd56b56841b66843
Carlos Salega <carlosalfredosalega@gmail.com>
2025-01-12 06:34:14 -0300

    chore(root): initialize nextjs tutorial 🎉

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Poppins, Roboto, Lusitana } from "next/font/google";
+
+export const poppins = Poppins({
+	weight: ["400", "500", "600", "800"],
+	style: "normal",
+});
+
+export const roboto = Roboto({
+	weight: ["400", "500", "700"],
+	style: "normal",
+});
+
+export const lusitana = Lusitana({
+	weight: ["400", "700"],
+	style: "normal",
+});
